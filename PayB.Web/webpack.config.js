c14e7046281fcf5c47943bf8004617675f945c31
Ranga Fernando <ranga.fernando@gmail.com>
2018-10-20 22:38:58 +1100

    configure compiling type script code with MSBuild

--- PayB.Web/webpack.config.js
+++ PayB.Web/webpack.config.js
@@ -1,32 +1,42 @@
 ﻿const path = require("path");
+const webpack = require("webpack");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    context: path.resolve(__dirname, "./Modules/Components/"),
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
+module.exports = function (env) {
 
-    entry: {
-        main: './index'
-    },
-    output: {
-        publicPath: './Assets/Scripts',
-        path: path.resolve(__dirname, './Assets/Scripts/'),
-        filename: '[name].build.js'
-    },
+    const isProduction = env === 'prod';
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
+    return {
+        context: path.join(__dirname, "./Modules/Components/"),
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".json"]
+        },
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "source-map",
+
+        entry: {
+            main: './index'
+        },
+
+        plugins: isProduction ? [new UglifyJsPlugin()] : [],
+
+        output: {
+            publicPath: './Assets/Scripts',
+            path: path.join(__dirname, './Assets/Scripts/'),
+            filename: isProduction ? '[name].build.min.js' : '[name].build.js'
+        },
+
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            ]
+        },
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDOM"
+        }
     }
 };
